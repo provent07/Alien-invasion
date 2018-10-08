a46a2426f05d4fe58ad9d4ed50874f442ee0b63d
Adriel Soengadi <iamsoengadi@gmail.com>
2018-10-08 23:12:11 +0700

    Alien invasion

---
+++ settings.py
@@ -0,0 +1,11 @@
+import pygame
+class settings():
+    def __init__(self):
+        self.screen_width=800
+        self.screen_height=600
+        self.bg_color= (255,255,255)
+        self.ship_speed_factor=1
+        self.bullet_speed_factor=1.25
+        self.bullet_width=10
+        self.bullet_height=10
+        self.bullet_color=(50,100,150)
